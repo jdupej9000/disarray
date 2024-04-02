c88cc6743da81a23613adae4889eba5ed723633b
jdupej9000 <jdupej9000@gmail.com>
2024-04-02 21:54:36 +0200

    Benchmark gathers a statistical sample.

--- src/Benchmark/bench.h
+++ src/Benchmark/bench.h
@@ -1,7 +1,17 @@
 #pragma once
 
 #include <chrono>
+#include <algorithm>
 
+struct benchtask
+{
+	void* fnptr;
+	uint64_t* times;
+	uint64_t repCount;
+	uint64_t setCount;
+};
+
+extern "C" uint64_t sample_fun_x64(benchtask* pbt);
 extern "C" uint64_t measure_fun2_x64(uint64_t reps, void* funPtr);
 
 namespace dsry::bench
@@ -12,6 +22,8 @@ namespace dsry::bench
 		double secsPerRepClean;
 		uint64_t dummy;
 	};
+
+	
 
 	template<typename TRet>
 	TRet blank(uint64_t x)
@@ -41,4 +53,31 @@ namespace dsry::bench
 
 		return ret;
 	};
+
+	void measure(void* dut, void* ctl = blank<uint32_t>)
+	{
+		constexpr uint64_t NumReps = 1048576;
+		constexpr uint64_t NumSets = 92;
+		uint64_t* tDut = new uint64_t[NumSets];
+		uint64_t* tCtl = new uint64_t[NumSets];
+
+		benchtask bt{};
+		bt.repCount = NumReps;
+		bt.setCount = NumSets;
+		
+		bt.times = tCtl;
+		bt.fnptr = ctl;
+		sample_fun_x64(&bt);
+
+		bt.times = tDut;
+		bt.fnptr = dut;
+		sample_fun_x64(&bt);
+
+		std::sort(tCtl, tCtl + NumSets);
+		std::sort(tDut, tDut + NumSets);
+
+
+
+		delete[](tDut, tCtl);
+	}
 };
--- src/Benchmark/bench_noopt.asm
+++ src/Benchmark/bench_noopt.asm
@@ -1,4 +1,5 @@
 public measure_fun2_x64
+public sample_fun_x64
 
 .data
 .code
@@ -9,6 +10,70 @@ public measure_fun2_x64
 ; must preserve : r12-15, rdi, rsi, rbx, rbp, ymm6-15
 ; 
 ; rdtsc -> edx:eax
+
+
+align 8
+sample_fun_x64 proc
+	
+	push		rsi
+	push		rdi
+	push		r12				; scratch
+	push		r13				; time accumulator
+	push		r14				; outer counter
+	push		r15
+
+	mov			rsi, rcx		; benchtask*
+	mov			rdi, [rsi]		; fnptr
+	xor			r14, r14
+	
+_sample_fun_x64_outer:
+	mov			r12, [rsi + 16]
+	xor			r13, r13
+
+_sample_fun_x64_inner:
+	mov			rcx, r12
+
+	rdtsc
+	shl			rdx, 32
+	or			rdx, rax
+	mov			r15, rdx
+
+	mov			rdx, r14
+	call		rdi
+
+	rdtsc						
+	shl			rdx, 32			
+	or			rdx, rax
+	xor			rcx, rcx
+	sub			rdx, r15
+	cmovo		rdx, rcx		
+	add			r13, rdx
+	
+	dec			r12
+	jnz			_sample_fun_x64_inner
+
+	mov			r12, [rsi + 8]	; benchtask->times
+	mov			[r12 + 8 * r14], r13
+	inc			r14
+	
+	mov			r12, [rsi + 24]
+	cmp			r14, r12
+	jb			_sample_fun_x64_outer
+
+	pop			r15
+	pop			r14
+	pop			r13
+	pop			r12
+	pop			rdi
+	pop			rsi
+
+	ret
+
+align 8
+sample_fun_x64 endp
+
+
+;;;
 
 align 8
 measure_fun2_x64 proc
