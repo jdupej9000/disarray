a43e1d51fd8f69b29763984cbaf699fa46d5c50b
Jan Dupej <jandupej@microsoft.com>
2024-04-19 17:11:56 +0200

    general swar operations

--- src/Common/miscmath.h
+++ src/Common/miscmath.h
@@ -7,4 +7,20 @@ namespace dsry::math
 	bool is_power_of_two(size_t x);
 	uint64_t gcd(uint64_t u, uint64_t v);
 	uint64_t lcm(uint64_t u, uint64_t v);
+
+	// Mark the MSB of each field. E.g. to add 8 Bytes, specify 0x8080808080808080.
+	template<typename TVal, TVal NMsbMask>
+	inline TVal swar_add(TVal a, TVal b) noexcept
+	{
+		// https://www.chessprogramming.org/SIMD_and_SWAR_Techniques
+		return ((a & ~NMsbMask) + (b & ~NMsbMask)) ^ ((a ^ b) & NMsbMask);
+	}
+
+	template<typename TVal, TVal NMsbMask>
+	inline TVal swar_sub(TVal a, TVal b) noexcept
+	{
+		return ((a | NMsbMask) - (b & ~NMsbMask)) ^ ((a ^ ~b) & NMsbMask);
+	}
+
+	// TODO: https://w3.pppl.gov/~hammett/work/2009/AIM-239-ocr.pdf
 };
